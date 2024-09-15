5ab9af70e3c24e17f8989814c89a5395c8e89425
Thiago Terra <thiagoterradev@gmail.com>
2024-09-15 18:49:01 -0300

    chore: adjustments to city creation method

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,28 +1,20 @@
 import { Request, Response } from "express";
 import * as yup from "yup";
 import { validation } from "../../shared/middleware";
+import { StatusCodes } from "http-status-codes";
 
 interface ICity {
   name: string;
-  state: string;
-}
-
-interface IFilter {
-  filter?: string;
 }
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<ICity>(yup.object().shape({
     name: yup.string().required().min(3),
-    state: yup.string().required().min(3),
-  })),
-  query: getSchema<IFilter>(yup.object().shape({
-    filter: yup.string().optional().min(3),
   })),
 }));
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
   console.log(req.body);
 
-  return res.send("Create!");
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Not implemented");
 };
