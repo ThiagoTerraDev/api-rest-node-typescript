8eadff8abe76884229d752fab7c16417fe76eb0f
Thiago Terra <thiagoterradev@gmail.com>
2024-09-26 01:00:39 -0300

    feat: implements tests to fetch a city by id

---
+++ tests/cities/GetById.test.ts
@@ -0,0 +1,33 @@
+import { StatusCodes } from "http-status-codes";
+import { testApp } from "../jest.setup";
+
+
+describe("Cities - GetById", () => {
+
+  it("Fetches a record by id", async () => {
+    
+    const firstResponse = await testApp
+      .post("/cities")
+      .send({ name: "Buenos Aires" });
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.CREATED);
+
+    const fetchedResponse = await testApp
+      .get(`/cities/${firstResponse.body}`)
+      .send();
+      
+    expect(fetchedResponse.statusCode).toEqual(StatusCodes.OK);
+    expect(fetchedResponse.body).toHaveProperty("name");
+  });
+
+
+  it("Tries to fetch a record that does not exist", async () => {
+    
+    const firstResponse = await testApp
+      .get("/cities/99999")
+      .send();
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(firstResponse.body).toHaveProperty("errors.default");
+  });
+});
