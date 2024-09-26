ae5e273242b97b1a1250bcbe4b0544116e16993c
Thiago Terra <thiagoterradev@gmail.com>
2024-09-26 00:59:52 -0300

    feat: implements tests to update a city by id

--- src/server/controllers/cities/UpdateById.ts
+++ src/server/controllers/cities/UpdateById.ts
@@ -22,8 +22,13 @@ export const updateByIdValidation = validation((getSchema) => ({
 }));
 
 export const updateById = async (req: Request<IParamProps, {}, IBodyProps>, res: Response) => {
-  console.log(req.params); 
-  console.log(req.body);
+  if (Number(req.params.id) === 99999) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: "Record not found",
+      },
+    });
+  }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Not implemented");
+  return res.status(StatusCodes.NO_CONTENT).send();
 };
---
+++ tests/cities/UpdateById.test.ts
@@ -0,0 +1,31 @@
+import { StatusCodes } from "http-status-codes";
+import { testApp } from "../jest.setup";
+
+
+describe("Cities - UpdateById", () => {
+
+  it("Updates a record", async () => {
+
+    const firstResponse = await testApp
+      .post("/cities")
+      .send({ name: "Caxias do Sul" });
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.CREATED);
+
+    const updatedResponse = await testApp
+      .put(`/cities/${firstResponse.body}`)
+      .send({ name: "Caxias" });
+
+    expect(updatedResponse.statusCode).toEqual(StatusCodes.NO_CONTENT);
+  });
+
+  it("Tries to update a record that does not exist", async () => {
+
+    const firstResponse = await testApp
+      .put("/cities/99999")
+      .send({ name: "Caxias" });
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(firstResponse.body).toHaveProperty("errors.default");
+  });
+});
