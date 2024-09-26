c6516de0a70b76704b7c208bc7e7e4dd7841421d
Thiago Terra <thiagoterradev@gmail.com>
2024-09-25 22:55:11 -0300

    feat: implements tests to create a new city

---
+++ tests/cities/Create.test.ts
@@ -0,0 +1,25 @@
+import { StatusCodes } from "http-status-codes";
+import { testApp } from "../jest.setup";
+
+
+describe("Cities - Create", () => {
+
+  it("Creates a record", async () => {
+
+    const firstResponse = await testApp
+      .post("/cities")
+      .send({ name: "São Paulo" });
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.CREATED);
+  });
+
+  it("Prevents the creation of a record with a very short name", async () => {
+
+    const firstResponse = await testApp
+      .post("/cities")
+      .send({ name: "Sã" });
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(firstResponse.body).toHaveProperty("errors.body.name");
+  });
+});
