c58c3793e03e76e49f7fbce52a66273c35d9e6ee
Thiago Terra <thiagoterradev@gmail.com>
2024-09-26 01:00:58 -0300

    feat: implements tests to get all cities

---
+++ tests/cities/GetAll.test.ts
@@ -0,0 +1,24 @@
+import { StatusCodes } from "http-status-codes";
+import { testApp } from "../jest.setup";
+
+
+
+describe("Cities - GetAll", () => {
+
+  it("Returns a list of all registered cities", async () => {
+
+    const firstResponse = await testApp
+      .post("/cities")
+      .send({ name: "Belo Horizonte" });
+
+    expect(firstResponse.statusCode).toEqual(StatusCodes.CREATED);
+
+    const fetchedResponse = await testApp
+      .get("/cities")
+      .send();
+
+    expect(Number(fetchedResponse.header["x-total-count"])).toBeGreaterThan(0);
+    expect(fetchedResponse.statusCode).toEqual(StatusCodes.OK);
+    expect(fetchedResponse.body.length).toBeGreaterThan(0);
+  });
+});
