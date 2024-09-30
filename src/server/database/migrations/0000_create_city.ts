ec5d9ab148d26bd988debe54c3247befd0d909bc
Thiago Terra <thiagoterradev@gmail.com>
2024-09-29 22:20:31 -0300

    feat: implements sqlite knex create table city

---
+++ src/server/database/migrations/0000_create_city.ts
@@ -0,0 +1,24 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+
+export async function up(knex: Knex) {
+  return knex
+    .schema
+    .createTable(ETableNames.city, table => { 
+      table.bigIncrements("id").primary().index();
+      table.string("name", 150).index().notNullable();
+
+      table.comment("Table to store the cities");
+    })
+    .then(() => console.log(`# Created table ${ETableNames.city}`));
+}
+
+
+export async function down(knex: Knex) {
+  return knex
+    .schema
+    .dropTable(ETableNames.city)
+    .then(() => 
+      console.log(`# Dropped table ${ETableNames.city}`));
+}
