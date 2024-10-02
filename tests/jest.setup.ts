331d5fea2c63987a4324d04853a70c6c7bfbe06c
Thiago Terra <thiagoterradev@gmail.com>
2024-10-02 18:45:38 -0300

    test: adds database for tests

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,14 @@
 import supertest from "supertest";
 
 import { app } from "../src/server/App";
+import { Knex } from "../src/server/database/knex";
+
+beforeAll(async () => {
+  await Knex.migrate.latest();
+});
+
+afterAll(async () => {
+  await Knex.destroy();
+});
 
 export const testApp = supertest(app);
