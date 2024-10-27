6c8e9c9a472c3eb804956017b017cf3f6d3fd9fa
Thiago Terra <thiagoterradev@gmail.com>
2024-10-27 20:50:39 -0300

    feat: adds all routes for members

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { CitiesController } from "./../controllers"; 
+import { CitiesController, MembersController } from "./../controllers"; 
 
 const router = Router();
 
@@ -9,13 +9,16 @@ router.get("/", (req, res) => {
 });
 
 router.get("/cities", CitiesController.getAllValidation, CitiesController.getAll);
-
 router.post("/cities", CitiesController.createValidation, CitiesController.create);
-
 router.get("/cities/:id", CitiesController.getByIdValidation, CitiesController.getById);
-
 router.put("/cities/:id", CitiesController.updateByIdValidation, CitiesController.updateById);
-
 router.delete("/cities/:id", CitiesController.deleteByIdValidation, CitiesController.deleteById);
+
+router.get("/members", MembersController.getAllValidation, MembersController.getAll);
+router.post("/members", MembersController.createValidation, MembersController.create);
+router.get("/members/:id", MembersController.getByIdValidation, MembersController.getById);
+router.put("/members/:id", MembersController.updateByIdValidation, MembersController.updateById);
+router.delete("/members/:id", MembersController.deleteByIdValidation, MembersController.deleteById);
+
 
 export { router };
