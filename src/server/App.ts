908c447260b97415334554cbbc2d1241cc7b755c
Thiago Terra <thiagoterradev@gmail.com>
2024-11-09 19:43:35 -0300

    feat: implements cors

--- src/server/App.ts
+++ src/server/App.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import { router } from "./routes";
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.ENABLED_CORS?.split(";") || [],
+}));
+
+// CORS future implementation
+
+// app.use(cors({
+//   origin: process.env.NODE_ENV === "production" 
+//     ? "https://myappfrontend.com" 
+//     : "http://localhost:3000",
+// }));
+
 
 app.use(express.json());
 
