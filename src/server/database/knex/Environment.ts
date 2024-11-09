6ee115f85f15cf25f47a61a4cc5d719a335abdd2
Thiago Terra <thiagoterradev@gmail.com>
2024-11-09 12:15:33 -0300

    feat: adds environment configs for production

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -28,5 +28,19 @@ export const test: Knex.Config = {
 };
 
 export const production: Knex.Config = {
-  ...development,
+  client: "pg",
+  migrations: {
+    directory: path.resolve(__dirname, "..", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "..", "seeds"),
+  },
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT) || 5432,
+    ssl: { rejectUnauthorized: false },
+  },
 };
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,6 +1,12 @@
 import { knex } from "knex";
+import "dotenv/config";
+import pg from "pg";
 import { development, test, production } from "./Environment";
 
+
+if (process.env.NODE_ENV === "production") {
+  pg.types.setTypeParser(20, "text", parseInt);
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {
